f7811923fa6890946da106c2af7da07723d3a595
NelvazzA21 <67016604+NelvazzA21@users.noreply.github.com>
2020-12-09 17:48:13 +0100

    Bot

--- commands/test.js
+++ commands/test.js
@@ -1,4 +1,3 @@
-const { timeStamp } = require('console');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
@@ -6,26 +5,6 @@ module.exports = {
     name: "test",
 
     async execute(message, args, bot) {
-        const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
-        if (args[1] === 'on') {
-            channels.forEach(channel => {
-                channel.updateOverwrite(message.guild.roles.everyone, {
-                    SEND_MESSAGES: false
-                }).then(() => {
-                    channel.setName(channel.name += `🔒`)
-                })
-            })
-            return message.channel.send('locked all channels');
-        } else if (args[1] === 'off') {
-            channels.forEach(channel => {
-                channel.updateOverwrite(message.guild.roles.everyone, {
-                    SEND_MESSAGES: true
-                }).then(() => {
-                        channel.setName(channel.name.replace('🔒', ''))
-                    }
-                )
-            })
-            return message.channel.send('unlocked all channels')
-        }
+        
     }
 }
